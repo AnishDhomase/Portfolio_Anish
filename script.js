b2d7cbf31ca1dbefb0c39ad1d6dfaa612e8af921
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2023-08-19 13:32:53 +0530

    Updated script.js with details

--- script.js
+++ script.js
@@ -125,3 +125,19 @@ window.addEventListener("load",function(){
     document.querySelector(".page-loader").style.display = "none";
     },1000);
 })
+
+
+// Form submit Button
+function EmailSend (){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
